2b698326bc0e4774dae7c17f0515e717df881615
Aznec <73877026+Aznec@users.noreply.github.com>
2020-11-13 15:55:26 +0100

    Update trie.py

--- trie.py
+++ trie.py
@@ -1,1 +1,15 @@
+def selectMin(list):
+    var=list[0]
+    for i in list:
+        if var>i:
+            var=i
+    return var
 
+def triselect(list):
+    result=[]
+    while len(list)>0:
+        var=selectMin(list)
+        
+    return result
+
+print(selectMin([9,4,5,6]))
